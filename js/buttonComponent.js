dfcc2015321efe0340ce8d7c82ee06e83c0eb517
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-03 15:12:13 -0500

    finishing up for the weekend. fully responsive sight with js component to toggle social icons on or off on click.

--- js/buttonComponent.js
+++ js/buttonComponent.js
@@ -1,24 +1,34 @@
 // BUTTON COMPONENT
 
+// CREATE BUTTON CLASS
 class Button {
   constructor(e) {
+    // assign this in object
     this.e = e;
     //console.log(e);
 
+    // select button class and assign to variable
     this.tBtn = document.querySelector(".social__toggle--btn");
-    console.log(this.tBtn);
+    //console.log(this.tBtn);
 
+    // assign classes to toggle to variable
     this.display = document.querySelectorAll(".toggleSocial");
 
+    // add event listener to new var from above
     this.tBtn.addEventListener("click", () => this.toggleThings());
   }
 
+  // method to toggle class
   toggleThings() {
-    const displays = document.querySelectorAll(".social__toggle--btn");
+    // var to seclect class button
+    const displays = document.querySelector(".social__toggle--btn");
 
+    // loop through above var and for each toggle class to be switched
     this.display.forEach(dis => dis.classList.toggle("toggleSocial"));
   }
 }
-let Buttons = document
+// SELECT ALL CLASSES .SOCIAL__TOGGLE--BTN AND ASSIGN THEM TO VAR BUTTONS LOOP
+// THROUGH THEM AND CREATE NEW BUTTON CLASS
+let buttons = document
   .querySelectorAll(".social__toggle--btn")
   .forEach(btn => new Button(btn));
